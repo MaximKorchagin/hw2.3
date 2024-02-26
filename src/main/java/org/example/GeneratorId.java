0c388dbfb11dc642b7f2d10909b7d85a18e23079
Maxim.Korchagin <Airfallsc2@gmail.com>
2024-02-26 08:57:54 +0300

    init 2.3

---
+++ src/main/java/org/example/GeneratorId.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class GeneratorId {
+    private final AtomicLong nextId = new AtomicLong(1);
+
+    private GeneratorId() {
+    }
+
+    private static class GeneratorIdHolder {
+        public static final GeneratorId generatorId = new GeneratorId();
+    }
+
+    public static GeneratorId getGeneratorId() {
+        return GeneratorIdHolder.generatorId;
+    }
+
+    public Long getId() {
+        return nextId.getAndIncrement();
+    }
+}
